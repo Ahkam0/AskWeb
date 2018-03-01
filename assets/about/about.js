bd0935c94ae4ef71298009db8e8798788763a89a
IR-Qureshi <ibrehman1995@gmail.com>
2018-03-01 08:14:09 -0500

    adding who we are and about us

--- assets/about/about.js
+++ assets/about/about.js
@@ -1,73 +1,143 @@
-jQuery(document).ready(function() {
-  var $current = $('.visible'),
-    animate = false;
+(function($) {
+ 
+  var SliceSlider = {
+    
+    /**
+     * Settings Object
+     */
+    settings: {
+      delta:              0,
+      currentSlideIndex:  0,
+      scrollThreshold:    40,
+      slides:             $('.slide'),
+      numSlides:          $('.slide').length,
+      navPrev:            $('.js-prev'),
+      navNext:            $('.js-next'),
+    },
+    
+    /**
+     * Init
+     */
+    init: function() {
+      s = this.settings;
+      this.bindEvents();
+    },
+    
+    /**
+     * Bind our click, scroll, key events
+     */
+    bindEvents: function(){
+      
+      // Scrollwheel & trackpad
+      s.slides.on({
+        'DOMMouseScroll mousewheel' : SliceSlider.nextSlide
+      });
+      // On click prev
+      s.navPrev.on({
+        'click' : SliceSlider.prevSlide
+      });
+      // On click next
+      s.navNext.on({
+        'click' : SliceSlider.nextSlide
+      });
+      // On Arrow keys
+      $(document).keyup(function(e) {
+        // Left or back arrows
+        if ((e.which === 37) ||  (e.which === 38)){
+          SliceSlider.prevSlide();
+        }
+        // Down or right
+        if ((e.which === 39) ||  (e.which === 40)) {
+          SliceSlider.nextSlide();
+        }
+      });
+     
+    },
+    
+    /** 
+     * Interept scroll direction
+     */
+    handleScroll: function(e){
 
-  function nextSlide() {
-    if (animate === false) {
-      animate = true;
+      // Scrolling up
+      if (e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0) { 
 
-      if ($current.index() + 1 == $('article').length) {
-        var $next = $('article:first-child');
-      } else {
-        $next = $current.next('article');
+        s.delta--;
+     
+        if ( Math.abs(s.delta) >= s.scrollThreshold) {
+          SliceSlider.prevSlide();
+        }
       }
-
-      $current.removeClass('visible');
-      $next.addClass('visible');
-      $current = $next;
-
-      window.location.hash = 'slide' + ($current.index() + 1);
-    }
-  }
-
-  function prevSlide() {
-    if (animate === false) {
-      animate = true;
-
-      if ($current.index() === 0) {
-        var $prev = $('article:last-child');
-      } else {
-        var $prev = $current.prev('article');
+ 
+      // Scrolling Down
+      else {
+ 
+        s.delta++;
+ 
+        if (s.delta >= s.scrollThreshold) {
+          SliceSlider.nextSlide();
+        }
       }
+ 
+      // Prevent page from scrolling
+      return false;
+    },
 
-      $current.removeClass('visible');
-      $prev.addClass('visible');
-      $current = $prev;
-      window.location.hash = 'slide' + ($current.index() + 1);
-    }
-  }
+    /**
+     * Show Slide
+     */
+    showSlide: function(){ 
+      // reset
+      s.delta = 0;
+      // Bail if we're already sliding
+      if ($('body').hasClass('is-sliding')){
+        return;
+      }
+      // Loop through our slides
+      s.slides.each(function(i, slide) {
 
-  $('#up').click(function() {
-    prevSlide();
+        // Toggle the is-active class to show slide
+        $(slide).toggleClass('is-active', (i === s.currentSlideIndex)); 
+        $(slide).toggleClass('is-prev', (i === s.currentSlideIndex - 1)); 
+        $(slide).toggleClass('is-next', (i === s.currentSlideIndex + 1)); 
+        
+        // Add and remove is-sliding class
+        $('body').addClass('is-sliding');
 
-    setTimeout(function() {
-      animate = false;
-    }, 1000);
-  });
-
-  $('#down').click(function() {
-    nextSlide();
-
-    setTimeout(function() {
-      animate = false;
-    }, 1000);
-  });
-
-  $('body').bind('mousewheel DOMMouseScroll MozMousePixelScroll', function(event) {
-    if (event.originalEvent.wheelDelta >= 0) {
-      prevSlide();
-
-      setTimeout(function() {
-        animate = false;
-      }, 1000);
-    } else {
-      if (animate == false) {
-        nextSlide();
-
-        setTimeout(function() {
-          animate = false;
+        setTimeout(function(){
+            $('body').removeClass('is-sliding');
         }, 1000);
+      });
+    },
+
+    /**
+     * Previous Slide
+     */
+    prevSlide: function(){
+      
+      // If on first slide, loop to last
+      if (s.currentSlideIndex <= 0) {
+        s.currentSlideIndex = s.numSlides;
       }
-    }
-  });
-});
+      s.currentSlideIndex--;
+      
+      SliceSlider.showSlide();
+    },
+
+    /**
+     * Next Slide
+     */
+    nextSlide: function(){
+      
+      s.currentSlideIndex++;
+   
+      // If on last slide, loop to first
+      if (s.currentSlideIndex >= s.numSlides) { 
+        s.currentSlideIndex = 0;
+      }
+ 
+      SliceSlider.showSlide();
+    },
+  };
+  SliceSlider.init();
+})(jQuery);
